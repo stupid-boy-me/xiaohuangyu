f0ce104aeea0c5cc774658bc6142ee9b7421844a
love_me_and_world <56495543+stupid-boy-me@users.noreply.github.com>
2023-01-31 10:04:21 +0800

    Update csv_data_processing.py

--- 山茶藏秋意/csv_data_processing.py
+++ 山茶藏秋意/csv_data_processing.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import tree
 
 
-df = pd.read_csv(r"你的csv路径")
+df = pd.read_csv(r"D:\yiguohuang\people\code_people\bank-full_2.csv")
 # print(df.info()) # RangeIndex: 45211 entries, 0 to 45210
 # 观察数据集信息
 # print(df.head())
@@ -37,8 +37,8 @@ x_dict_list = x.to_dict(orient='records')
 # 将类别转换成了one-hot编码
 dict_vec = DictVectorizer(sparse=False)
 x = dict_vec.fit_transform(x_dict_list)
-print("*" * 30 + " onehot编码 " + "*" * 30)
-print("*" * 30 + " 编码成功 " + "*" * 30)
+print("*" * 30 + " onehot Coding " + "*" * 30)
+print("*" * 30 + " Coding success " + "*" * 30)
 # print(dict_vec.get_feature_names())
 # print(x[:5])
 
@@ -53,10 +53,10 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
 '''
 dec_tree = tree.DecisionTreeClassifier()
 dec_tree.fit(x_train, y_train)
-print("model-1-决策树分类器")
-print("*" * 30 + " 准确率 " + "*" * 30)
+print("model-1-DecisionTreeClassifier")
+print("*" * 30 + " accuracy " + "*" * 30)
 print(dec_tree.score(x_test, y_test))
-
+print("model-1-DecisionTreeClassifier is over")
 '''
 第五步：model-2-KNN
 '''
@@ -65,8 +65,9 @@ knn.fit(x_train, y_train)      #放入训练数据进行训练
 #评估模型的得分
 score=knn.score(x_test,y_test)
 print("model-2-KNN")
-print(score)
-
+print("*" * 30 + " accuracy " + "*" * 30)
+print("model-2-KNeighborsClassifier accuracy is ：",score)
+print("model-2-KNN is over")
 '''
 第五步：model-3-随机森林分类器
        n_jobs: -1表示设置为核心数量
@@ -80,13 +81,14 @@ param = {
 }
 # 2折交叉验证
 search = GridSearchCV(rf, param_grid=param, cv=2)
-print("model-3-随机森林分类器")
-print("*" * 30 + " 超参数网格搜索 " + "*" * 30)
+print("model-3- RandomForestClassifier")
+print("*" * 30 + " Hyperparametric grid search " + "*" * 30)
 
 start_time = time.time()
 search.fit(x_train, y_train)
-print("耗时：{}".format(time.time() - start_time))
-print("最优参数：{}".format(search.best_params_))
+print("Time consuming：{}".format(time.time() - start_time))
+print("Optimal parameters：{}".format(search.best_params_))
 
-print("*" * 30 + " 准确率 " + "*" * 30)
+print("*" * 30 + " accuracy " + "*" * 30)
 print(search.score(x_test, y_test))
+print("model-3- RandomForestClassifier is over")
